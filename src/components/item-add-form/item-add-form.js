6cfaebee8ef642347e9d6741e14419cfde0471dc
ddemydenko <ddemydenko@luxoft.com>
2019-01-08 22:52:07 +0200

    make field required

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -29,7 +29,7 @@ export default class ItemAddForm extends Component {
   render() {
     return (
       <form className={'item-add-form d-flex'} onSubmit={this.onSubmit}>
-        <input className={'form-control'} type='text' onChange={this.onLabelChange}
+        <input required={true} className={'form-control'} type='text' onChange={this.onLabelChange}
         placeholder='What needs to be done'
         value={this.state.label}/>
         <button className={'btn btn-outline-secondary'}>
